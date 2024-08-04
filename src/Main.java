4c667f522ccfa32ff92abdab94721bd4d3dc16cb
picronicro <89722414+picronicro@users.noreply.github.com>
2024-08-04 17:31:33 +0600

    Made if statement in permission() a little prettier. Completed other tasks such as print the output and make a randomizer method generateRandomAge().

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
 
         System.out.println(permission(21, -10));
         System.out.println(permission(19, 28));
         System.out.println(permission(46, -10));
+        System.out.println(permission(16, 29));
+        System.out.println(permission(999, -6969));
+
+        System.out.println(); // небольшой отступ
+        System.out.println(permission(generateRandomAge(), 10));
+        System.out.println(permission(generateRandomAge(), 25));
 
     }
 
     public static String permission(int age, float temp) {
-        if ((age >= 20 && age <= 45) && (temp >= -20 && temp <= 30)) {
-            return "Можно идти гулять 1";
-        } else if (age < 20 && (temp >= 0 && temp <= 28)) {
-            return "Можно идти гулять 2";
-        } else if (age > 45 && (temp >= -10 && temp <= 25)) {
-            return "Можно идти гулять 3";
+        System.out.println("Возраст: " + age + ", темп: " + temp);
+        if ((age >= 20 && age <= 45) && (temp >= -20 && temp <= 30) ||
+                age < 20 && (temp >= 0 && temp <= 28) ||
+                age > 45 && (temp >= -10 && temp <= 25)) {
+            return "Можно идти гулять";
         } else {
             return "Оставайтесь дома";
         }
     }
+
+    public static int generateRandomAge() {
+        return new Random().nextInt(100);
+    }
+
 }
